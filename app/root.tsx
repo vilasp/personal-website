6a4b2c85d8dcf95484cecabc48cdd9e8f1a0a1cd
Villiam Aspegren <villiam.aspegren@hemnet.se>
2022-07-30 19:09:04 +0200

    feat: change title of website

--- app/root.tsx
+++ app/root.tsx
@@ -17,7 +17,7 @@ import styles from './tailwind.css'
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
-  title: 'New Remix App',
+  title: 'Villiam Aspegren',
   viewport: 'width=device-width,initial-scale=1',
 })
 
