d5160412ae830451a894db7164a82e6c4ce4c0e7
Villiam Aspegren <villiam.aspegren@hemnet.se>
2023-02-26 13:36:55 +0100

    feat: remove placeholder text

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,66 +1,7 @@
 export default function Index() {
   return (
-    <main style={{fontFamily: 'system-ui, sans-serif', lineHeight: '1.4'}}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      sdlkfjslkdfjklsd
-    </main>
+    <main
+      style={{fontFamily: 'system-ui, sans-serif', lineHeight: '1.4'}}
+    ></main>
   )
 }
